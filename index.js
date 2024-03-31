8f31d2ba52f2378eb738a4b1bcfa39bdb4b594db
Sahil Kumar <120113586+sahilkumar100@users.noreply.github.com>
2024-03-31 20:33:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+var rect = document.querySelector(".center");
+
+rect.addEventListener('mousemove', function(details) {
+    var rectangleLocation = rect.getBoundingClientRect();
+    var insideRectVal = details.clientX - rectangleLocation.left;
+    
+    if (insideRectVal < rectangleLocation.width / 2) {
+        var redColor = gsap.utils.mapRange(
+            0, rectangleLocation.width / 2, 255, 0, insideRectVal
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redColor}, 0, 0)`,
+            ease: "power4.inOut",
+        });
+        
+    } else {
+        var blueColor = gsap.utils.mapRange(
+            rectangleLocation.width / 2,
+            rectangleLocation.width,
+            0,
+            255,
+            insideRectVal
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease: "power4.inOut",
+        });
+    }
+});
+
+rect.addEventListener("mouseleave", function() {
+    gsap.to(rect, {
+        backgroundColor: "white",
+    });
+});
+
